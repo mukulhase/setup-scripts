ab28e7e653b18851ebb6882ddcaf5d43829917d9
Mukul Hase <mukulhase@gmail.com>
2020-04-13 02:24:58 +0530

    update

--- powerline-fonts.sh
+++ powerline-fonts.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/src/github.com/powerline/
 cd ~/src/github.com/powerline/
 rm -rf fonts
-git clone git@github.com:powerline/fonts.git
+git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
--- ubuntu-desktop-packages.sh
+++ ubuntu-desktop-packages.sh
@@ -1,3 +1,6 @@
+## VS Code
+sudo snap install --classic code
+
 ## Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -;
 sudo apt-get install -y apt-transport-https;
